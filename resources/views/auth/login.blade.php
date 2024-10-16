212e92544712675ab50c51cdedaded951b32931b
bintangandk <124556644+bintangandk@users.noreply.github.com>
2024-10-16 16:49:29 +0700

    delete link to register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -93,9 +93,6 @@
                 </form>
               </div>
             </div>
-            <div class="mt-5 text-muted text-center">
-              Don't have an account? <a href="#">Create One</a>
-            </div>
           </div>
         </div>
       </div>
