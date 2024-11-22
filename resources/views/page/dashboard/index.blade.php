ac2cd7e647f13152fea226825f6bd33fe41be37f
Syahroni12 <ronixria@gmail.com>
2024-11-22 23:43:46 +0700

    rvisi profile dan tambah halaman setting otomatis berdasarkan apa

--- resources/views/page/dashboard/index.blade.php
+++ resources/views/page/dashboard/index.blade.php
@@ -8,8 +8,9 @@
                     <div class="col-lg-6 col-md-4 col-sm-12" id="alat-{{ $item['id'] }}">
                         <div class="card card-statistic-2 d-flex flex-column" style="height: 250px;">
                             <div class="card-stats-title text-center" style="padding-top: 15px;">
-                                <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" style="width: 50px; height: 50px;">
-                                <h4> Alat {{ $item["id"] }}</h4>
+                                <img src="{{ asset('assets/img/logo.png') }}" alt="Logo"
+                                    style="width: 50px; height: 50px;">
+                                <h4> Alat {{ $item['id'] }}</h4>
                             </div>
                             <div class="row">
                                 <div class="col-md-6" style="padding-left: 80px;">
@@ -19,8 +20,9 @@
                                             style="width: 35px; height: 35px;">
                                     </div>
                                     <div class="card-wrap">
-                                        <div class="card-body" style="font-size: 2rem;" id="temperature-{{ $item['id'] }}">
-                                            {{ $item["nilai_temperature"]->nilai_temperature ?? "-" }}
+                                        <div class="card-body" style="font-size: 2rem;"
+                                            id="temperature-{{ $item['id'] }}">
+                                            {{ $item['nilai_temperature']->nilai_temperature ?? '-' }}
                                         </div>
                                         <div class="card-header">
                                             <h4 style="font-size: 1rem;">Suhu</h4>
@@ -37,7 +39,7 @@
                                             style="width: 35px; height: 35px;">
                                     </div>
                                     <div class="card-body" style="font-size: 2rem;" id="humidity-{{ $item['id'] }}">
-                                        {{ $item["nilai_humidity"]->nilai_humidity  ?? "-" }}
+                                        {{ $item['nilai_humidity']->nilai_humidity ?? '-' }}
                                     </div>
                                     <div class="card-header">
                                         <h4 style="font-size: 1rem;">Kelembapan</h4>
@@ -56,24 +58,27 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        function fetchData() {
-            $.ajax({
-                url: '/fetch-data',
-                type: 'GET',
-                success: function(response) {
-                    response.forEach(item => {
-                        $('#temperature-' + item.id).text(item.nilai_temperature);
-                        $('#humidity-' + item.id).text(item.nilai_humidity);
-                    });
-                }
-            });
-        }
+    <script>
+        $(document).ready(function() {
+            function fetchData() {
+                $.ajax({
+                    url: '/fetch-data',
+                    type: 'GET',
+                    success: function(response) {
+                        response.forEach(item => {
+                            $('#temperature-' + item.id).text(item.nilai_temperature
+                                .nilai_temperature);
+                            $('#humidity-' + item.id).text(item.nilai_humidity.nilai_humidity);
+                        });
 
-        // Polling setiap 5 detik
-        setInterval(fetchData, 5000);
-    });
-</script>
+                        // console.log(response);
 
+                    }
+                });
+            }
+
+            // Polling setiap 5 detik
+            setInterval(fetchData, 5000);
+        });
+    </script>
 @endsection
