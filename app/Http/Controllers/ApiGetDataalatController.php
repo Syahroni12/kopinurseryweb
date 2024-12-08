b3badb4e527f3026b57c8bd1711c44d4c4091aa8
Agung Kurniawan <agungklewang26@gmail.com>
2024-12-08 21:31:53 +0700

    update

--- app/Http/Controllers/ApiGetDataalatController.php
+++ app/Http/Controllers/ApiGetDataalatController.php
@@ -259,7 +259,7 @@ class ApiGetDataalatController extends Controller
                 'file',
                 file_get_contents($file->getPathname()), // Gunakan getPathname()
                 $file->getClientOriginalName()
-            )->post('http://127.0.0.1:8585/predict/');
+            )->post('http://192.168.1.18:8585/predict/');
 
             // Periksa respons dari FastAPI
             if ($response->successful()) {
